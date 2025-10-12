2fda8e8d49e007bd5a6cc43c3cde65e2b3271a69
salamah551 <umshihabs@gmail.com>
2025-10-12 00:34:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // IMPORTANT: do NOT set `output: 'export'`. We need SSR on Vercel.
+  // experimental: { appDir: true } // (not required; App Router is already active)
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
