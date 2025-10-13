5b9e57fc004bc6bf6f96c3038f98d865dc92357e
salamah551 <umshihabs@gmail.com>
2025-10-12 22:37:45 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-// app/page.js (SERVER COMPONENT)
+// app/page.js
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 import LandingClient from './landing-client';
 
 export default function Page() {
-  // Render the Client UI inside a Server wrapper so Next doesn't try to prerender "/"
   return <LandingClient />;
 }
